833793ccc15c49ab68148710bbc1bc352f52b031
Thet Hlaing Phyo <91230275+tphyo@users.noreply.github.com>
2022-10-31 09:31:21 -0700

    comment deleted

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -25,7 +25,6 @@ class ViewController: UIViewController {
     //comment
     //another comment
     //second comment
-    //third comment
     
     override func viewDidLoad() {
         super.viewDidLoad()
